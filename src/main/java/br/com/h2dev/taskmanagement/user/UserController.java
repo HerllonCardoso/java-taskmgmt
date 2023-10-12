c5df9249972d081c2aad7b192e28008e9d431eec
Herllon <cardoso.herllon@gmail.com>
2023-10-12 13:23:42 -0300

    feat: encrypt password

--- src/main/java/br/com/h2dev/taskmanagement/user/UserController.java
+++ src/main/java/br/com/h2dev/taskmanagement/user/UserController.java
@@ -3,6 +3,9 @@ package br.com.h2dev.taskmanagement.user;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,6 +25,11 @@ public class UserController {
             System.out.println("User already exists");
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("User already exists");
         }
+
+        var passwordHashed = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+
+        userModel.setPassword(passwordHashed);
+
         var userCreated = this.userRepository.save(userModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
