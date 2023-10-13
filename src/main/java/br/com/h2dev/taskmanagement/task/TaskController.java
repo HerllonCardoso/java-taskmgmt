f0cc62fbc2c4c40f00a6e06978b42d522938fbda
Herllon <cardoso.herllon@gmail.com>
2023-10-13 06:47:26 -0300

    feat: task controller and repo

---
+++ src/main/java/br/com/h2dev/taskmanagement/task/TaskController.java
@@ -0,0 +1,40 @@
+package br.com.h2dev.taskmanagement.task;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@RestController
+@RequestMapping("/tasks")
+
+public class TaskController {
+    @Autowired
+    private ITaskRepository taskRepository;
+
+    @PostMapping("/")
+    public ResponseEntity create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
+        var task = this.taskRepository.save(taskModel);
+        var idUser = request.getAttribute("idUser");
+
+        var currentDate = LocalDateTime.now();
+        if (currentDate.isAfter(taskModel.getStartAt()) || currentDate.isAfter(taskModel.getEndAt())) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body("The start date or end date should be greater than the current date");
+
+        }
+
+        taskModel.setIdUser((UUID) idUser);
+        return ResponseEntity.status(HttpStatus.OK).body(task);
+    }
+
+}
