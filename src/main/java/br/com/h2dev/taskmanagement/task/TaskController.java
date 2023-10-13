0e42d48f9559f4b8df0afd0ece94210b8552356f
Herllon <cardoso.herllon@gmail.com>
2023-10-13 07:14:20 -0300

    feat: update partial

--- src/main/java/br/com/h2dev/taskmanagement/task/TaskController.java
+++ src/main/java/br/com/h2dev/taskmanagement/task/TaskController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.h2dev.taskmanagement.utils.Utils;
 import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
@@ -58,10 +59,13 @@ public class TaskController {
 
     @PutMapping("/{id}")
     public TaskModel list(@RequestBody TaskModel taskModel, @PathVariable UUID id, HttpServletRequest request) {
-        var idUser = request.getAttribute("idUser");
-        taskModel.setIdUser((UUID) idUser);
-        taskModel.setId(id);
-        var tasks = this.taskRepository.save(taskModel);
+
+        var task = this.taskRepository.findById(id).orElse(null);
+
+        Utils.copyNonNullProperties(taskModel, task);
+
+        var tasks = this.taskRepository.save(task);
+        
         return tasks;
     }
 }
